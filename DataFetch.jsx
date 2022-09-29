a290ae79d25deadd03c17be3d1e53fc6aaef72de
sneha shinde <Sneha06032000@gmail.com>
2022-09-29 21:45:28 +0530

    project

---
+++ DataFetch.jsx
@@ -0,0 +1,27 @@
+import React,{useState,useEffect} from "react";
+import axios from "axios";
+
+export function DataFetch (){
+    const [posts, setPosts] = useState([]);
+
+    useEffect(()=>{
+        axios
+        .get("https://jsonplaceholder.typicode.com/comments")
+        .then((response)=> {
+            console.log(response.data)
+            setPosts([...response.data]);
+        });
+},[]);
+return ( 
+<div>
+    {posts.map((post)=>(
+        <div key={post.id}>
+            <h1>Name :{post.name}</h1>
+            <p>Body :{post.body}</p>
+            </div>
+    ))}
+        
+</div>
+      
+      )
+};
